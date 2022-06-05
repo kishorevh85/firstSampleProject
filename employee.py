95b4567f431995b899ba96f6af7d4f6e1152ade0
kishorevh85 <kishorevh85@gmail.com>
2022-06-05 12:13:23 +0530

    added dob param in __init__() changed the obj creation

--- employee.py
+++ employee.py
@@ -1,10 +1,12 @@
 class employee:
-    def __init__(self, name, age):
+    def __init__(self, name, age, dob):
         self.name = name
         self.age = age
+        self.dob = dob
 
     def show(self):
-        print(f'The object details - name - {self.name} age - {self.age}')
+        print(f'The object details - name - {self.name} age - {self.age}, dob = {self.dob}')
 
-emp = employee('kishore',22)
+
+emp = employee(name='kishore', age=22, dob='30/11/1985')
 emp.show()
